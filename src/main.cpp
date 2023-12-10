6d438763735b49b6565ee0fe3b6a613201025a85
wuruidong <wuruidong@hotmail.com>
2023-12-10 18:12:47 +0800

    Add compiler flags

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
 #include "Hello.h"
 
 int main(int argc, char *argv[])
 {
     Hello hi;
     hi.print();
+
+    #ifdef EX2
+    std::cout << "Compile flag EX2" << std::endl;
+    #endif
+
+    #ifdef EX3
+    std::cout << "Compile flag EX3" << std::endl;
+    #endif
     return 0;
 }
