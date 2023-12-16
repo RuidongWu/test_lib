81a0fba78ea868e5ae6910f7a8871dc12546e08b
wuruidong <wuruidong@hotmail.com>
2023-12-17 00:48:58 +0800

    Add configure_file in cfg_file

---
+++ cfg_file/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ver.h"
+#include "path.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "hello Version " << ver << endl;
+    cout << "Path is " << path << endl;
+    return 0;
+}
