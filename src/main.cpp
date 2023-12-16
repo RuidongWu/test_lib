6dfd27a4defe95e8e35a2d30c4a33ca121e06b84
wuruidong <wuruidong@hotmail.com>
2023-12-16 23:59:30 +0800

    Complete 01 basic

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include "Hello.h"
+#include <boost/shared_ptr.hpp>
+#include <boost/filesystem.hpp>
 
 int main(int argc, char *argv[])
 {
     Hello hi;
     hi.print();
 
-    #ifdef EX2
-    std::cout << "Compile flag EX2" << std::endl;
-    #endif
-
     #ifdef EX3
     std::cout << "Compile flag EX3" << std::endl;
     #endif
+
+    boost::shared_ptr<int> isp(new int(4));
+    boost::filesystem::path path = "usr/share/cmake/modules";
+    if(path.is_relative())
+    {
+        std::cout << "path is relative" << std::endl;
+    }
+    else
+    {
+        std::cout << "path is not a relative" << std::endl;
+    }
+
     return 0;
 }
