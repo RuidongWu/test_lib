c398bb83aa9ecfa0fbe3e520cbfe7aaa6dd1b7c6
wuruidong <wuruidong@hotmail.com>
2023-12-17 00:26:26 +0800

    Add sub directories with cmake test

---
+++ sub/main.cpp
@@ -0,0 +1,12 @@
+#include "sub1/sublib1.h"
+#include "sub2/sublib2.h"
+
+int main()
+{
+    sublib1 hi;
+    hi.print();
+
+    sublib2 howdy;
+    howdy.print();
+    return 0;
+}
---
+++ sub1/inc/sub1/sublib1.h
@@ -0,0 +1,10 @@
+#ifndef __SUBLIB_1_H
+#define __SUBLIB_1_H
+
+class sublib1
+{
+public:
+    void print();
+};
+
+#endif
